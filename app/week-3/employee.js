ed959ef7acc65084a28576f31c1af27e39e79978
Rudra Pratap <rudrapratap@edu.sait.ca>
2025-02-04 09:18:31 -0700

    new week

--- app/week-3/employee.js
+++
@@ -1,9 +0,0 @@
-export default function Employee(){
-    return(
-        <section className= "bg-pink-200 p-4 m-4"> 
-        <p>name: {employee1.name}</p>
-        <p>Age: {employee1.age}</p>
-        <p>salary: {employee1.salary}</p>
-        </section>
-    )
-}
